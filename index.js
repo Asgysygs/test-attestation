f79900d82d8a5918b1f335b100254d0d4bebc559
Lo0lode <arthassas@mail.ru>
2023-05-22 22:47:02 +0300

    step1

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import _ from 'lodash'
 
 export default function solution(content){
   // BEGIN
-
+  const splitContent = content.split('\n').slice(1,-1)
+  splitContent.map((elen)=>{
+    // console.log(splitContent.length)
+    elen = elen.trim().split(',')
+    console.log(elen)
+  })
   // END
 
 }
